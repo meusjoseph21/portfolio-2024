ac57f0199c94fcfa7cb7e74160369c3d720971e3
Joseph Meus <joseph@chekspend.com>
2024-02-23 14:33:21 -0800

    about section complete

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import AboutSection from "./components/AboutSection";
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
       <NavBar />
       <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       
       </div>
     
